9605b74123287367fad5e48f9b43e0fe3926a34c
Tushar-Mahata <kumar15-7252@diu.edu.bd>
2023-04-30 05:56:54 +0600

    using layer class

--- layers.py
+++ layers.py
@@ -1,11 +1,23 @@
 import numpy as np
 
+np.random.seed(0)
+
 X = [[1, 2, 3, 2.5],
      [3, -2, -1, .01],
      [0.0, 0.3, -9, -1]]
 
 class Layer_Dense:
-    def __init__(self):
-        pass
-    def forward(self):
-        pass
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.baises = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.baises
+
+layer1 = Layer_Dense(4, 5)
+layer1.forward(X)
+
+print(layer1.output)
+
+layer2 = Layer_Dense(5, 2)
+layer2.forward(layer1.output)
+print(layer2.output)
